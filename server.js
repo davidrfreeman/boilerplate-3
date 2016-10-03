021b20ed0fda2d32587357176691a6b053f26f27
David Freeman <davidfreeman@holos.solutions>
2016-10-02 21:45:35 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+// Create our app
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use((req, res, next) => {
+  return (req.headers['x-forwarded-proto'] === 'https') ? res.redirect('http://' + req.hostname + req.url) : next();
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('*', function(req, res) {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, function () {
+  console.log('Express server is up on port ' + PORT);
+});
